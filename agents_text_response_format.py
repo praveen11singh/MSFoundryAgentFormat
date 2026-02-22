34850a16be56df6548cdfd1dc94a8403312a7c32
praveen kumar <praveen11_singh@yahoo.co.in>
2026-02-22 14:46:12 +0000

    Initial commit

---
+++ agents_text_response_format.py
@@ -0,0 +1,50 @@
+import os
+from azure.ai.projects import AIProjectClient
+from azure.identity import DefaultAzureCredential
+from azure.ai.agents.models import ListSortOrder, AgentsResponseFormat, RunStatus
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+project_client = AIProjectClient(
+    endpoint=os.environ["PROJECT_ENDPOINT"],
+    credential=DefaultAzureCredential(),
+)
+
+with project_client:
+    agents_client = project_client.agents
+
+    agent = agents_client.create_agent(
+        model=os.environ["MODEL_DEPLOYMENT_NAME"],
+        name="my-agent",
+        instructions="You are helpful agent.",
+        response_format=AgentsResponseFormat(type="text"),
+    )
+    print(f"Created agent, agent ID: {agent.id}")
+
+    thread = agents_client.threads.create()
+    print(f"Created thread, thread ID: {thread.id}")
+
+    threads = agents_client.threads.list()
+
+    message = agents_client.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content="Hello, give me a list of planets in our solar system, and their mass in kilograms.",
+    )
+    print(f"Created message, message ID: {message.id}")
+
+    run = agents_client.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
+
+    if run.status != RunStatus.COMPLETED:
+        print(f"The run did not succeed: {run.status=}.")
+
+    agents_client.delete_agent(agent.id)
+    print("Deleted agent")
+
+    messages = agents_client.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
+    for msg in messages:
+        if msg.text_messages:
+            last_text = msg.text_messages[-1]
+            print(f"{msg.role}: {last_text.text.value}")
